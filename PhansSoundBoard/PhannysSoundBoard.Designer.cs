19a6beefb44f44842f716b141561d64c6b9a866b
Eric Sheris <esheris@live.com>
2015-08-04 19:27:24 -0700

    blah

--- PhansSoundBoard/PhannysSoundBoard.Designer.cs
+++ PhansSoundBoard/PhannysSoundBoard.Designer.cs
@@ -34,12 +34,12 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
-            this.btn4 = new System.Windows.Forms.Button();
             this.btn3 = new System.Windows.Forms.Button();
-            this.btn6 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
             this.btn5 = new System.Windows.Forms.Button();
-            this.btn8 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
             this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
@@ -50,7 +50,7 @@
             this.btn1.Name = "btn1";
             this.btn1.Size = new System.Drawing.Size(75, 23);
             this.btn1.TabIndex = 1;
-            this.btn1.Text = "btn1";
+            this.btn1.Text = "button1";
             this.btn1.UseVisualStyleBackColor = true;
             this.btn1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -93,65 +93,65 @@
             this.toolStripButton1.Text = "toolStripButton1";
             this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
             // 
-            // btn4
-            // 
-            this.btn4.Location = new System.Drawing.Point(13, 81);
-            this.btn4.Name = "btn4";
-            this.btn4.Size = new System.Drawing.Size(75, 23);
-            this.btn4.TabIndex = 5;
-            this.btn4.Text = "button3";
-            this.btn4.UseVisualStyleBackColor = true;
-            this.btn4.Click += new System.EventHandler(this.btn4_Click);
-            // 
             // btn3
             // 
-            this.btn3.Location = new System.Drawing.Point(117, 81);
+            this.btn3.Location = new System.Drawing.Point(13, 81);
             this.btn3.Name = "btn3";
             this.btn3.Size = new System.Drawing.Size(75, 23);
-            this.btn3.TabIndex = 4;
-            this.btn3.Text = "button4";
+            this.btn3.TabIndex = 3;
+            this.btn3.Text = "button3";
             this.btn3.UseVisualStyleBackColor = true;
-            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            this.btn3.Click += new System.EventHandler(this.btn4_Click);
             // 
-            // btn6
+            // btn4
             // 
-            this.btn6.Location = new System.Drawing.Point(13, 120);
-            this.btn6.Name = "btn6";
-            this.btn6.Size = new System.Drawing.Size(75, 23);
-            this.btn6.TabIndex = 7;
-            this.btn6.Text = "button5";
-            this.btn6.UseVisualStyleBackColor = true;
-            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            this.btn4.Location = new System.Drawing.Point(117, 81);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(75, 23);
+            this.btn4.TabIndex = 4;
+            this.btn4.Text = "button4";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn3_Click);
             // 
             // btn5
             // 
-            this.btn5.Location = new System.Drawing.Point(117, 120);
+            this.btn5.Location = new System.Drawing.Point(13, 120);
             this.btn5.Name = "btn5";
             this.btn5.Size = new System.Drawing.Size(75, 23);
-            this.btn5.TabIndex = 6;
-            this.btn5.Text = "button6";
+            this.btn5.TabIndex = 5;
+            this.btn5.Text = "button5";
             this.btn5.UseVisualStyleBackColor = true;
-            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            this.btn5.Click += new System.EventHandler(this.btn6_Click);
             // 
-            // btn8
+            // btn6
             // 
-            this.btn8.Location = new System.Drawing.Point(13, 162);
-            this.btn8.Name = "btn8";
-            this.btn8.Size = new System.Drawing.Size(75, 23);
-            this.btn8.TabIndex = 9;
-            this.btn8.Text = "button7";
-            this.btn8.UseVisualStyleBackColor = true;
-            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            this.btn6.Location = new System.Drawing.Point(117, 120);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(75, 23);
+            this.btn6.TabIndex = 6;
+            this.btn6.Text = "button6";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn5_Click);
             // 
             // btn7
             // 
-            this.btn7.Location = new System.Drawing.Point(117, 162);
+            this.btn7.Location = new System.Drawing.Point(13, 162);
             this.btn7.Name = "btn7";
             this.btn7.Size = new System.Drawing.Size(75, 23);
-            this.btn7.TabIndex = 8;
-            this.btn7.Text = "button8";
+            this.btn7.TabIndex = 7;
+            this.btn7.Text = "button7";
             this.btn7.UseVisualStyleBackColor = true;
-            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            this.btn7.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Location = new System.Drawing.Point(117, 162);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(75, 23);
+            this.btn8.TabIndex = 8;
+            this.btn8.Text = "button8";
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn7_Click);
             // 
             // PhannysSoundBoard
             // 
@@ -159,12 +159,12 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Lime;
             this.ClientSize = new System.Drawing.Size(754, 530);
-            this.Controls.Add(this.btn8);
             this.Controls.Add(this.btn7);
-            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn8);
             this.Controls.Add(this.btn5);
-            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn6);
             this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn4);
             this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.btn2);
             this.Controls.Add(this.btn1);
@@ -187,12 +187,12 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.ToolStrip toolStrip1;
         private System.Windows.Forms.ToolStripButton toolStripButton1;
-        private System.Windows.Forms.Button btn4;
         private System.Windows.Forms.Button btn3;
-        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn4;
         private System.Windows.Forms.Button btn5;
-        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn6;
         private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
     }
 }
 
